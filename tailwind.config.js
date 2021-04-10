465c0d621c7d0524232790cfd5abb19fc18d0e20
Perlaki András <cbsdzh@inf.elte.hu>
2021-04-10 21:35:15 +0200

    initial config finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      serif: ['IM Fell English', 'serif'],
+      /*smallCaps: ['IM Fell Great Primer SC', 'serif'],*/
+    },
     extend: {},
   },
   variants: {
